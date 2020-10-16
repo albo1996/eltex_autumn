71a92c445bb71e030036979cd1f9b47fb1e7990c
albo1996 <borovikav96@gmail.com>
2020-10-17 00:32:54 +0700

    Add files via upload

---
+++ C/pointers/ptr_home1.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+
+int main()
+{
+    int a = 14031996;
+    char* ptr;
+
+    ptr = &a;
+
+    printf("1:%d\n", *ptr);//Первый байт
+    ptr++;
+
+    printf("2:%d\n", *ptr);//Второй байт. Выводим и меняем его значение.
+    *ptr=12;
+    ptr++;
+
+    printf("3:%d\n", *ptr);//Третий байт
+    ptr++;
+
+    printf("4:%d\n", *ptr);//Четвёртый байт
+
+    printf("A = %d\n", a);//Выводим новое значение A
+
+}
---
+++ C/pointers/ptr_home2.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+
+int main()
+{   
+    char a = 'A';
+    char b = 'B';
+    char* ptr1;
+    char** ptr2;
+
+    ptr1=&a;
+
+    ptr2=&ptr1;
+    
+    *ptr2=&b;
+
+    printf("*ptr1 -> %c\n", *ptr1);
+    
+}
