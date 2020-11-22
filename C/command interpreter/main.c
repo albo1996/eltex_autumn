0bbf8d416b15ac4c0ab1fa656f6836c0100b2569
albo1996 <borovikav96@gmail.com>
2020-11-23 01:33:16 +0700

    Add files via upload

---
+++ C/command interpreter/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+
+int main()
+{
+    pid_t pid;
+    extern int errno;
+    int status;
+    int cmp;
+    char *argv[2];
+    char path[]="/bin/";
+    char command[50];
+    
+    while(1)
+    {
+    	printf("$ ");
+    	scanf("%50s", command);
+    	
+    	cmp = strcmp("exit", command);
+    	
+    	if (cmp == 0)
+    	{
+    		exit(0);
+    	}
+    	
+    	argv[0] = (char*)malloc(strlen(path) + strlen(command) + 1);
+    	strcpy(argv[0], path);
+    	strncat(argv[0], command, strlen(command));
+    	
+    	pid = fork();
+    	
+    	if (pid == 0)
+    	{
+    		execv(argv[0], argv);
+    		
+    		if (errno == ENOENT)
+    		{
+    			printf("Комманда '%s' не найдена!\n", command);
+    		}
+    		
+    		exit(1);
+    	}
+    	else
+    	{
+    		waitpid(pid, &status, __WALL);
+    		free(argv[0]);
+    	}
+    }
+    return 0;
+}
