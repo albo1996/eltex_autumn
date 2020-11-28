b94258827bac59f180bc4c928873cf1d7935515d
albo1996 <borovikav96@gmail.com>
2020-11-28 22:58:03 +0700

    Update purchases.c

--- C/threads_shop/purchases.c
+++ C/threads_shop/purchases.c
@@ -7,10 +7,6 @@ void* purchases(void* param)
 	
 	while(1)
 	{
-		pthread_mutex_t mutex;
-		
-		pthread_mutex_init(&mutex, NULL);
-
 		j = random_number(0, 4);
 
 		if (store[j] == 0)
@@ -19,7 +15,7 @@ void* purchases(void* param)
 			continue;
 		}
 
-		pthread_mutex_lock(&mutex);
+		pthread_mutex_lock(&mutex[j]);
 
 		if (need >= store[j])
 		{
@@ -32,14 +28,12 @@ void* purchases(void* param)
 			need = 0;		
 		}
 
-		pthread_mutex_unlock(&mutex);
+		pthread_mutex_unlock(&mutex[j]);
 		
-		pthread_mutex_destroy(&mutex);
-
 		if(need == 0)
 		{
 			(*(int*)param)++;
 			break;
 		}
 	}	
-}	
+}
